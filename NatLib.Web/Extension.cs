25d92dfc6a64fb489458eb02b462f897bd9696b2
nat052010 <jonathanblumapas@gmail.com>
2017-07-28 15:53:05 +0800

    NatLib.Web

---
+++ NatLib.Web/Extension.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace NatLib.Web
+{
+    public static class Extension
+    {
+        public static Dictionary<string, string> GetQueryStrings(this HttpRequestMessage request)
+        {
+            var result = request.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
+        }
+
+        public static string GetQueryString(this HttpRequestMessage request, string key)
+        {
+            var match = request.GetQueryStrings().FirstOrDefault(r => r.Key.ToLower() == key.ToLower());
+            return match.Value;
+        }
+
+        /// <summary>
+        /// Returns an individual HTTP Header value
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetHeader(this HttpRequestMessage request, string key)
+        {
+            IEnumerable<string> keys = null;
+            if (!request.Headers.TryGetValues(key, out keys))
+                return null;
+
+            return keys.First();
+        }
+
+        /// <summary>
+        /// Retrieves an individual cookie from the cookies collection
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cookieName"></param>
+        /// <returns></returns>
+        public static string GetCookie(this HttpRequestMessage request, string cookieName)
+        {
+            CookieHeaderValue cookie = request.Headers.GetCookies(cookieName).FirstOrDefault();
+            if (cookie != null)
+                return cookie[cookieName].Value;
+
+            return null;
+        }
+
+        public static bool PageExists(this string address)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
+                request.Method = WebRequestMethods.Http.Head;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+            catch
+            {   
+                return false;
+            }
+        }
+
+    }
+}
